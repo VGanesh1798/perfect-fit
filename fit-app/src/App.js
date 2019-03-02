df4210e3c850c6a97eb483543367de174bb0e895
Areeb Ahmed <areebahmed916@gmail.com>
2019-03-02 01:12:43 -0600

    added modified app.css, js, and package-lock

--- fit-app/src/App.js
+++ fit-app/src/App.js
@@ -1,25 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  handleClick() {
+    alert('hi')
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="button">
+          <button id='Soccer' className="Sports" onClick={this.handleClick}>Soccer</button>
+          <button className="Sports">Football</button>
+          <button className="Sports">Baseball</button>
+        </div>
       </div>
     );
   }
